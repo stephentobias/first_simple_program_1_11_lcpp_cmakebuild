5cbc52cfb8f40aae2906855e11c51cba9cf6824b
stephentobias <200682130+stephentobias@users.noreply.github.com>
2025-03-01 15:30:13 -0600

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,10 @@
+﻿// first_simple_program_1_11_lcpp_cmakebuild.cpp : Defines the entry point for the application.
+//
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello CMake." << '\n';
+	return 0;
+}
